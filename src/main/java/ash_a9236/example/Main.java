0330773f044d7b77bc6bc2bb6b504a4b18e24c7c
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-09-10 08:57:39 -0400

    is this working 01

--- src/main/java/ash_a9236/example/Main.java
+++ src/main/java/ash_a9236/example/Main.java
@@ -2,6 +2,6 @@ package ash_a9236.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("is this thing working 01");
     }
 }
