35bfaaee17c6df091671871e0a236f86c46d3d20
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-09-10 11:37:08 -0400

    creation of constructors, getters, and setters for CreditCardPayment()

--- src/main/java/ash_a9236/example/CreditCardPayment.java
+++ src/main/java/ash_a9236/example/CreditCardPayment.java
@@ -1,7 +1,50 @@
 package ash_a9236.example;
 
-public class CreditCardPayment {
+public class CreditCardPayment extends Payment {
+    private String fName;
+    private String lName;
+    private int expirationDate;
+    private int creditCardNum;
 
+    public CreditCardPayment(double paymentAmount, String currency, String fName, String lName, int expirationDate, int creditCardNum) {
+        super(paymentAmount, currency);
+        this.fName = fName;
+        this.lName = lName;
+        this.expirationDate = expirationDate;
+        this.creditCardNum = creditCardNum;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(int expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getCreditCardNum() {
+        return creditCardNum;
+    }
+
+    public void setCreditCardNum(int creditCardNum) {
+        this.creditCardNum = creditCardNum;
+    }
 
 /*______________________________________________________________________________________________________________________
 Question 01
