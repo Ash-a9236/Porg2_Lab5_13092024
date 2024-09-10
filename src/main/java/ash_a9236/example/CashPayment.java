6c6d4ba9160c0cd52f1328c23565b7f1f6881002
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-09-10 09:51:05 -0400

    add class CashPayment() extends Payment()

---
+++ src/main/java/ash_a9236/example/CashPayment.java
@@ -0,0 +1,21 @@
+package ash_a9236.example;
+
+public class CashPayment extends Payment {
+
+/*______________________________________________________________________________________________________________________
+Question 01
+
+Next, define a class named CashPayment that is derived from Payment. This class should redefine the paymentDetails
+method to indicate that the payment is in cash. Include appropriate constructor(s).
+
+Define a class named CreditCardPayment that is derived from Payment. This class should contain instance variables
+for the name on the card, expiration date, and credit card number.
+
+Include appropriate constructor(s). Finally, redefine the paymentDetails method to include all credit card information
+in the printout.
+
+Create a main method that creates at least two CashPayment and two CreditCardPayment objects with different values
+and calls paymentDetails for each.
+*/
+
+}
